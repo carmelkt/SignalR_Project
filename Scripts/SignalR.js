301d7f2ddbe2622e69ff654a1c0b175b9be3c0fd
carmelkt <carmel@qburst.com>
2021-01-15 15:39:18 +0530

    Init

---
+++ Scripts/SignalR.js
@@ -0,0 +1,15 @@
+﻿//alert("TESTING!! It works!!!")
+
+
+(function () {
+    $.connection.hub.start()
+        .done(function () {
+            console.log("TESTING!! It Works!!");
+            $.connection.myHub.server.announce("Connected!!");
+        })
+        .fail(function () { alert("ERROR!") });
+
+    $.connection.myHub.client.announce = function (message) {
+        $("#welcome-messages").append(message + "<br/>");
+    }
+})()
---
+++ Views/Shared/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRTest.Views.Shared
+{
+    public class MyHub:Hub
+    {
+        public void Announce(string message)
+        {
+            Clients.All.Announce(message);
+        }
+    }
+}
